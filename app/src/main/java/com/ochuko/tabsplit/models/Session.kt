bcd02687e5c70ac57db9dcce63a8998a83b607d0
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-02 12:22:09 +0100

    update: add session model

--- app/src/main/java/com/ochuko/tabsplit/models/Session.kt
+++ app/src/main/java/com/ochuko/tabsplit/models/Session.kt
@@ -1,10 +1,13 @@
 package com.ochuko.tabsplit.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Session(
     val id: String,
     val title: String,
     val description: String,
     val currency: String,
+    @SerializedName("invite_code")
     val inviteCode:String? = null,
     val owner: Participant,
     val inviteUrl:String? = null
