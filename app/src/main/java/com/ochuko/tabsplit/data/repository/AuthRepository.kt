2d3d40f7a4458b44df41c4fb107c0e8b5fd97bc3
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-02 14:36:36 +0100

    fix: update login

--- app/src/main/java/com/ochuko/tabsplit/data/repository/AuthRepository.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/repository/AuthRepository.kt
@@ -1,29 +1,44 @@
 package com.ochuko.tabsplit.data.repository
 
 
-
 import android.content.Context
 
 import com.ochuko.tabsplit.data.api.AuthApi
 import com.ochuko.tabsplit.data.api.LoginRequest
+import com.ochuko.tabsplit.data.api.LoginResponse
+import com.ochuko.tabsplit.data.api.RegisterRequest
 import com.ochuko.tabsplit.data.local.SecurePrefs
 import com.ochuko.tabsplit.models.User
 
 
-class AuthRepository(private val api:AuthApi, private val ctx: Context) {
+class AuthRepository(private val api: AuthApi, private val ctx: Context) {
 
     private val prefs = ctx.getSharedPreferences("auth", Context.MODE_PRIVATE)
 
-    suspend fun login(email: String, password: String): User? {
+
+    suspend fun login(email: String, password: String): Pair<User, String>? {
         val res = api.login(LoginRequest(email, password))
 
-        if(res.isSuccessful){
-            res.body()?.token?.let {saveToken(it)}
+        if (res.isSuccessful) {
+            val (body, token) = res.body() ?: return null
+            val user = User(body.id, body.username, body.email, body.zaddr)
+
+            return Pair(user, token)
+        }
+
+        return null;
+    }
+
+    suspend fun signup(email: String, password: String): User? {
+        val res = api.register(RegisterRequest(email, password))
+
+        if (res.isSuccessful) {
+//            res.body().?.token?.let {saveToken(it)}
 
             res.body()
         }
 
-        return  null;
+        return null;
     }
 
 
