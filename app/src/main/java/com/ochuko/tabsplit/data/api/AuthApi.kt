d6c9d286dc55d9149ae2a457a1ff986dbaf1f1fb
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-02 15:41:45 +0100

    fix: add data class

--- app/src/main/java/com/ochuko/tabsplit/data/api/AuthApi.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/api/AuthApi.kt
@@ -8,11 +8,12 @@ import retrofit2.http.*
 
 data class LoginRequest(val email: String, val password: String)
 data class  RegisterRequest(val email: String, val password: String)
+data class AuthResponse(val user: User,  val toke:String)
 
 interface AuthApi {
     @POST("/auth/login")
-    suspend fun login(@Body credentials: LoginRequest): Response<User>
+    suspend fun login(@Body credentials: LoginRequest): Response<AuthResponse>
 
     @POST("/auth/register")
-    suspend fun register(@Body request: RegisterRequest): Response<User>
+    suspend fun register(@Body request: RegisterRequest): Response<AuthResponse>
 }
