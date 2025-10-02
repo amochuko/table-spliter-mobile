e089d4a12ef374eb01b924aa8a5e031995717d21
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-02 12:31:12 +0100

    update: add screen

--- app/src/main/java/com/ochuko/tabsplit/MainActivity.kt
+++ app/src/main/java/com/ochuko/tabsplit/MainActivity.kt
@@ -1,36 +1,19 @@
 package com.ochuko.tabsplit
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ochuko.tabsplit.ui.theme.TabSplitTheme
-import android.graphics.Bitmap
-import android.widget.ImageView
-import com.google.zxing.BarcodeFormat
-import com.journeyapps.barcodescanner.BarcodeEncoder
-import com.google.zxing.WriterException
-import com.google.zxing.qrcode.QRCodeWriter
-import kotlinx.coroutines.yield
-
-import android.graphics.Color
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.unit.dp
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ochuko.tabsplit.ui.auth.LoginScreen
+import com.ochuko.tabsplit.ui.screens.sessions.SessionScreen
+import com.ochuko.tabsplit.ui.screens.sessions.SessionDetailsScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -40,67 +23,26 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TabSplitTheme {
-                QRCodeScreen()
+                MyApp()
             }
         }
     }
 }
+
 
 @Composable
-fun QRCodeScreen() {
-    var text by remember { mutableStateOf("hello world") }
+fun MyApp() {
+    val navController = rememberNavController()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
-    ) {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        NavHost(navController = navController, startDestination = "login") {
+            composable("login") { LoginScreen(navController) }
+            composable("sessions") { SessionScreen(navController) }
+            composable("sessionDetails/{sessionId}") { backStackEntry ->
+                val sessionId = backStackEntry.arguments?.getString("sessionId") ?: ""
 
-        val qrBitmap = remember(text) { generateQRCode(text) }
-
-        qrBitmap?.let {
-            Image(
-                bitmap = it.asImageBitmap(),
-                contentDescription = "QR Code",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(300.dp)
-            )
-        }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { text = "https://another-invite-link.com" }) {
-            Text("Generate Another QR")
-        }
-    }
-}
-
-
-fun generateQRCode(text: String): Bitmap? {
-    return try {
-        val writer = QRCodeWriter()
-
-        val bitMatrix = writer.encode(text, BarcodeFormat.QR_CODE, 512, 512)
-        val width = bitMatrix.width
-        val height = bitMatrix.height
-
-        val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
-
-        for (x in 0 until width) {
-            for (y in 0 until height) {
-                bmp.setPixel(
-                    x, y, if (bitMatrix[x, y]) Color.BLACK else android
-                        .graphics.Color.WHITE
-                )
-
+                SessionDetailsScreen(navController, sessionId)
             }
         }
-
-        bmp
-    } catch (e: WriterException) {
-        e.printStackTrace()
-        null
     }
 }
