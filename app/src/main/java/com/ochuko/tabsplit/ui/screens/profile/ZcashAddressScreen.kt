1ac040e31fd35cb81528c6123803b12f45c546fe
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-03 10:22:51 +0100

    update: add Zcash Address screen

---
+++ app/src/main/java/com/ochuko/tabsplit/ui/screens/profile/ZcashAddressScreen.kt
@@ -0,0 +1,10 @@
+package com.ochuko.tabsplit.ui.screens.profile
+
+
+import androidx.compose.runtime.Composable
+import com.ochuko.tabsplit.ui.components.ui.ZcashAddressSettings
+
+@Composable
+fun ZcashAddressScreen() {
+    ZcashAddressSettings()
+}
