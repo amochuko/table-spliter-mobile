c3636d568cd430999d0dc34006363e05ad0ea1e9
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-03 10:20:15 +0100

    update: add about screen

---
+++ app/src/main/java/com/ochuko/tabsplit/ui/screens/about/About.kt
@@ -0,0 +1,24 @@
+package com.ochuko.tabsplit.ui.screens.about
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Text
+
+@Composable
+fun AboutScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "About screen",
+            fontSize = 16.sp,
+            color = Color.White
+        )
+    }
+}
