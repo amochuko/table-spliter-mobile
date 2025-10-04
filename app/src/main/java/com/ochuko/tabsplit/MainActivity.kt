f68cc6d7f39d7ca988a37a9e901ff127d4bd89d3
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-04 10:15:15 +0100

    fix: refactor AppNavHost

--- app/src/main/java/com/ochuko/tabsplit/MainActivity.kt
+++ app/src/main/java/com/ochuko/tabsplit/MainActivity.kt
@@ -4,18 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
 import com.ochuko.tabsplit.ui.theme.TabSplitTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.ochuko.tabsplit.ui.join.JoinSessionScreen
-import com.ochuko.tabsplit.ui.screens.auth.LoginScreen
-import com.ochuko.tabsplit.ui.screens.auth.SignupScreen
-import com.ochuko.tabsplit.ui.screens.sessions.SessionScreen
-import com.ochuko.tabsplit.ui.screens.sessions.SessionDetailsScreen
+import com.ochuko.ui.navigation.tabsplit.AppNavHost
 
 class MainActivity : ComponentActivity() {
 
@@ -25,83 +16,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TabSplitTheme {
-                MyApp()
-            }
-        }
-    }
-}
+                val navController = rememberNavController()
 
-
-@Composable
-fun MyApp() {
-    val navController = rememberNavController()
-
-    Surface(color = MaterialTheme.colorScheme.background) {
-        NavHost(navController = navController, startDestination = "login") {
-            composable("login") {
-                LoginScreen(
-                    onLoginSuccess = {
-                        navController.navigate("session") {
-                            popUpTo("Login") { inclusive = true }
-                        }
-                    },
-                    onSignupClick = { navController.navigate("signup") })
-            }
-
-            composable("signup") {
-                SignupScreen(
-                    onSignupSuccess = {
-                        navController.navigate("session") {
-                            popUpTo("signup") { inclusive = true }
-                        }
-                    },
-                    onLoginClick = { navController.navigate("login") }
-                )
-            }
-
-            composable("sessions") {
-                SessionScreen(
-                    onSessionClick = { sessionId ->
-                        navController.navigate("sessionDetails/$sessionId")
-                    },
-                    onCreateSession = { newSession ->
-                        navController.navigate("sessionDetails/${newSession.id}")
-                    },
-                    onRequireAuth = {
-                        // navigate to login
-                        navController.navigate("LoginScreen")
-                    }
-
-                )
-            }
-
-            // Session Details
-            composable("sessionDetails/{sessionId}") { backStackEntry ->
-                val sessionId = backStackEntry.arguments?.getString("sessionId") ?: ""
-                SessionDetailsScreen(navController, sessionId)
-            }
-
-            // Join Session
-            composable("join/{inviteCode}") { backStackEntry ->
-                val inviteCode = backStackEntry.arguments?.getString("inviteCode") ?: ""
-                JoinSessionScreen(
-                    inviteCode = inviteCode,
-                    onJoinSuccess = { session ->
-                        navController.navigate("sessionDetails/${session.id}") {
-                            popUpTo("join/$inviteCode") { inclusive = true }
-                        }
-                    },
-                    onAuthRequired = {
-                        navController.navigate("login") {
-                            popUpTo("join/$inviteCode") { inclusive = true }
-                        }
-                    },
-                    onJoinFailed = {
-                        navController.navigate("sessions") {
-                            popUpTo("join/$inviteCode") { inclusive = true }
-                        }
-                    }
-                )
+                AppNavHost(navController)
             }
         }
     }
