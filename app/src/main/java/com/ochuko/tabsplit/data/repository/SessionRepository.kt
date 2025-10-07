b7be4b758566cee0369d5d6bdb7f81f664bdbf23
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-07 21:17:26 +0100

    update: expand return type

--- app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
@@ -9,10 +9,11 @@ import com.ochuko.tabsplit.models.Session
 class SessionRepository(private val api: SessionApi) {
 
     suspend fun getSessions(): List<Session> {
-        val response = api.getSessions()
+        val res = api.getSessions()
 
-        if (response.isSuccessful) return response.body() ?: emptyList()
-        throw Exception("Failed to fetch sessions: ${response.code()}")
+        if (res.isSuccessful) return res.body()?.sessions ?: emptyList()
+
+        throw Exception("Failed to fetch sessions: ${res.code()}")
     }
 
     suspend fun getSession(id: String): Session? {
@@ -21,16 +22,10 @@ class SessionRepository(private val api: SessionApi) {
     }
 
     suspend fun createSession(req: SessionRequest): Session? {
-        Log.i("SessionRepository1", "Request: $req")
 
         val res = api.createSession(req)
 
-        Log.i("SessionRepository2", "Code: ${res.code()} | Success: ${res.isSuccessful}")
-        Log.i("SessionRepository3", "Body: ${res.body()}")
-        Log.i("SessionRepository4", "ErrorBody: ${res.errorBody()?.string()}")
-        Log.i("SessionRepository5", "Raw: ${res.raw()}")
-
-        return if (res.isSuccessful) res.body() else null
+        return if (res.isSuccessful) res.body()?.session else null
     }
 
     suspend fun joinByInvite(code: String): Session? {
