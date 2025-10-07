c36e40d6f8b9e65712fc9ec59136e01486590162
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-07 21:15:55 +0100

    update: expand return type

--- app/src/main/java/com/ochuko/tabsplit/models/Session.kt
+++ app/src/main/java/com/ochuko/tabsplit/models/Session.kt
@@ -1,17 +1,22 @@
 package com.ochuko.tabsplit.models
 
 import com.google.gson.annotations.SerializedName
+import java.util.Date
 
 data class Session(
     val id: String,
     val title: String,
     val description: String,
     val currency: String,
+    val qrDatatUrl: String? = null,
+    val inviteUrl: String? = null,
+
     @SerializedName("invite_code")
     val inviteCode: String? = null,
+
     @SerializedName("created_by")
     val createdBy: String,
+
     @SerializedName("created_at:")
-    val createdAt: String,
-    val inviteUrl: String? = null
+    val createdAt: Date
 )
