29607fa6989de812fe3a3a9ef0a4b2f4b08b6ec5
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-07 18:46:18 +0100

    update: clean up

--- app/src/main/java/com/ochuko/tabsplit/data/repository/UserRepository.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/repository/UserRepository.kt
@@ -1,18 +1,11 @@
 package com.ochuko.tabsplit.data.repository
 
-
-import com.ochuko.tabsplit.data.api.ApiClient
 import com.ochuko.tabsplit.data.api.UserApi
 import com.ochuko.tabsplit.models.User
-import com.ochuko.tabsplit.utils.Config
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-class UserRepository(private val api: UserApi) {
-
-    companion object {
-        val userApi = ApiClient.create<UserApi>(baseUrl = Config.API_BASE_URL)
-    }
+class UserRepository(private val api: UserApi, ) {
 
     suspend fun updateZAddr(zaddr: String): User? = withContext(Dispatchers.IO) {
         val res = api.updateZAddr(mapOf("zaddr" to zaddr))
