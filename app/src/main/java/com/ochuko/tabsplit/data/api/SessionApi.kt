d907d8251e6306de095b6fe5b84532d1b7535570
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-08 13:24:08 +0100

    update: expand type

--- app/src/main/java/com/ochuko/tabsplit/data/api/SessionApi.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/api/SessionApi.kt
@@ -1,6 +1,8 @@
 package com.ochuko.tabsplit.data.api
 
 import com.google.gson.annotations.SerializedName
+import com.ochuko.tabsplit.models.Expense
+import com.ochuko.tabsplit.models.Participant
 import com.ochuko.tabsplit.models.Session
 import retrofit2.Response
 import retrofit2.http.*
@@ -10,8 +12,39 @@ data class SessionsResponse(
     val sessions: List<Session>?
 )
 
+data class SessionOwner(
+    val id: String,
+    val username: String,
+    val zaddr: String,
+
+    @SerializedName("user_id")
+    val userIdr: String,
+)
+
+data class SessionWithOwner(
+    val id: String,
+    val title: String,
+    val description: String,
+    val currency: String,
+
+    @SerializedName("created_at")
+    val createdAt: String,
+
+    val owner: SessionOwner,
+)
+
+data class SessionOwnerResponse(
+    val participants: List<Participant>,
+    val expenses: List<Expense>,
+    val session: SessionWithOwner
+)
+
 data class SessionResponse(
-    @SerializedName("session") val session: Session?
+    val session: Session
+)
+
+data class SessionParticipantsResponse(
+    val participants: List<Participant>?
 )
 
 data class SessionRequest(val title: String, val description: String?)
@@ -23,7 +56,10 @@ interface SessionApi {
     suspend fun getSessions(): Response<SessionsResponse>
 
     @GET("/sessions/{id}")
-    suspend fun getSession(@Path("id") id: String): Response<SessionResponse>
+    suspend fun getSession(@Path("id") id: String): Response<SessionOwnerResponse>
+
+    @GET("/sessions/{sessionId}/participants")
+    suspend fun getParticipants(@Path("sessionId") sessionId: String): Response<SessionParticipantsResponse>
 
     @POST("/sessions")
     suspend fun createSession(@Body body: SessionRequest): Response<SessionResponse>
