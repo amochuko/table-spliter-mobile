71ce666df2e63732a4c04e5561546af80eb568e8
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-08 17:13:16 +0100

    update: implement addExpense

--- app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
@@ -1,10 +1,13 @@
 package com.ochuko.tabsplit.data.repository
 
+import com.ochuko.tabsplit.data.api.AddExpenseResponse
 import com.ochuko.tabsplit.data.api.JoinRequest
 import com.ochuko.tabsplit.data.api.SessionRequest
 import com.ochuko.tabsplit.data.api.SessionApi
 import com.ochuko.tabsplit.data.api.SessionOwnerResponse
+import com.ochuko.tabsplit.models.AddExpenseRequest
 import com.ochuko.tabsplit.models.Session
+import android.util.Log
 
 class SessionRepository(private val api: SessionApi) {
 
@@ -26,6 +29,14 @@ class SessionRepository(private val api: SessionApi) {
         val res = api.createSession(req)
 
         return if (res.isSuccessful) res.body()?.session else null
+    }
+
+    suspend fun addExpenses(sessionId: String, req: AddExpenseRequest): AddExpenseResponse? {
+
+        val res = api.addExpenses(sessionId, req)
+        Log.d("SessionRepo:addExpenses", "Code: ${res.code()}, Body: ${res.body()}")
+
+        return if (res.isSuccessful) res.body() else null
     }
 
     suspend fun joinByInvite(code: String): Session? {
