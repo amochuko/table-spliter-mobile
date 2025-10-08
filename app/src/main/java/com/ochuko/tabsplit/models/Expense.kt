0ec192f3d245d4e8a7caa5919ab1f337693ae967
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-08 17:13:44 +0100

    update: expand type

--- app/src/main/java/com/ochuko/tabsplit/models/Expense.kt
+++ app/src/main/java/com/ochuko/tabsplit/models/Expense.kt
@@ -1,12 +1,32 @@
 package com.ochuko.tabsplit.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Expense(
     val id: String,
+
+    @SerializedName("session_id")
     val sessionId: String,
+
     val memo: String,
     val amount: Double,
+
+    @SerializedName("payer_id")
     val payerId: String,
-    val createdAt: Long = System.currentTimeMillis(),
+
+    @SerializedName("created_at")
+    val createdAt: String,
+
+    @SerializedName("payer_username")
+    val payerUsername: String,
+
+    @SerializedName("payer_participant_id")
+    val payerParticipantId: String,
+)
+
+data class AddExpenseRequest(
+    val memo: String,
+    val amount: Double,
 )
 
 
