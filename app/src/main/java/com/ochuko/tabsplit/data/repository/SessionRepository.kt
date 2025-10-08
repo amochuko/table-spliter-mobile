5f5adcaa184679b69650bfe4c3717efeadfdd603
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-08 13:25:09 +0100

    fix: change return type

--- app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/repository/SessionRepository.kt
@@ -3,7 +3,11 @@ package com.ochuko.tabsplit.data.repository
 import com.ochuko.tabsplit.data.api.JoinRequest
 import com.ochuko.tabsplit.data.api.SessionRequest
 import com.ochuko.tabsplit.data.api.SessionApi
+import com.ochuko.tabsplit.data.api.SessionOwner
+import com.ochuko.tabsplit.data.api.SessionOwnerResponse
+import com.ochuko.tabsplit.models.Participant
 import com.ochuko.tabsplit.models.Session
+import android.util.Log
 
 class SessionRepository(private val api: SessionApi) {
 
@@ -15,9 +19,9 @@ class SessionRepository(private val api: SessionApi) {
         throw Exception("Failed to fetch sessions: ${res.code()} -> ${res.errorBody()}")
     }
 
-    suspend fun getSession(id: String): Session? {
+    suspend fun getSession(id: String): SessionOwnerResponse? {
         val res = api.getSession(id)
-        return if (res.isSuccessful) res.body()?.session else null
+        return if (res.isSuccessful) res.body() else null
     }
 
     suspend fun createSession(req: SessionRequest): Session? {
@@ -30,5 +34,10 @@ class SessionRepository(private val api: SessionApi) {
     suspend fun joinByInvite(code: String): Session? {
         val res = api.joinByInvite(JoinRequest(code))
         return if (res.isSuccessful) res.body() else null
+    }
+
+    suspend fun getParticipants(sessionId: String): List<Participant> {
+        val res = api.getParticipants(sessionId)
+        return if (res.isSuccessful) res.body()?.participants ?: emptyList() else emptyList()
     }
 }
