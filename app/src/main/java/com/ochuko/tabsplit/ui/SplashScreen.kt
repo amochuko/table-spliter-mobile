06319138d5d6ba52d226728fef75fcd0878a0ab0
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-21 09:30:59 +0100

    update: move to ui

--- app/src/main/java/com/ochuko/tabsplit/ui/SplashScreen.kt
+++ app/src/main/java/com/ochuko/tabsplit/ui/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.ochuko.tabsplit.ui.screens.splash
+package com.ochuko.tabsplit.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.FastOutSlowInEasing
