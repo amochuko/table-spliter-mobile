64a688fca4e6e5c9a257ead2182c400ad0dce1d3
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-21 21:16:09 +0100

    update: add extension to session

---
+++ app/src/main/java/com/ochuko/tabsplit/data/model/SessionMapper.kt
@@ -0,0 +1,48 @@
+package com.ochuko.tabsplit.data.model
+
+import com.ochuko.tabsplit.data.api.SessionOwner
+import com.ochuko.tabsplit.data.api.SessionWithOwner
+
+fun Session.toSessionWithOwner(owner: SessionOwner?=null):SessionWithOwner{
+    return SessionWithOwner(
+        id = id,
+        title = title,
+        description = description,
+        currency = currency,
+        createdAt = createdAt.toString(),
+        owner = owner ?: SessionOwner(
+            id = createdBy,
+            username = "Unknown",
+            zaddr = "",
+            userId = createdBy
+        )
+    )
+}
+
+fun Session.toFullSession(owner: SessionOwner? = null): FullSession {
+    return FullSession(
+        id = id,
+        title = title,
+        description = description,
+        currency = currency,
+        createdAt = createdAt.toString(),
+        createdBy = createdBy,
+        owner = owner,
+        inviteCode = inviteCode,
+        inviteUrl = inviteUrl,
+        qrDataUrl = qrDataUrl
+
+    )
+}
+
+fun SessionWithOwner.toFullSession(): FullSession {
+    return FullSession(
+        id = id,
+        title = title,
+        description = description,
+        currency = currency,
+        createdAt = createdAt,
+        createdBy = owner.userId,
+        owner = owner
+    )
+}
