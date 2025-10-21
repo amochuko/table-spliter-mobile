0c62e8d2796f1a8bcbe00a46c989a6e60b974462
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-21 14:52:13 +0100

    update: move to mapping

--- app/src/main/java/com/ochuko/tabsplit/data/model/Session.kt
+++ app/src/main/java/com/ochuko/tabsplit/data/model/Session.kt
@@ -1,9 +1,7 @@
 package com.ochuko.tabsplit.data.model
 
 import com.google.gson.annotations.SerializedName
-import com.ochuko.tabsplit.data.api.SessionOwner
 import java.util.Date
-import com.ochuko.tabsplit.data.api.SessionWithOwner
 
 data class Session(
     val id: String,
@@ -26,20 +24,3 @@ data class Session(
     @SerializedName("created_at")
     val createdAt: Date
 )
-
-
-fun Session.toSessionWithOwner(owner: SessionOwner?=null):SessionWithOwner{
-    return SessionWithOwner(
-        id = id,
-        title = title,
-        description = description,
-        currency = currency,
-        createdAt = createdAt.toString(),
-        owner = owner ?: SessionOwner(
-            id = createdBy,
-            username = "Unknown",
-            zaddr = "",
-            userId = createdBy
-        )
-    )
-}
