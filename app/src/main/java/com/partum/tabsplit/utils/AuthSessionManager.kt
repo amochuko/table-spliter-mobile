db06736e5e8c3f1eaae304ffb4967cf8b0de1c62
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-25 06:54:11 +0100

    update: refactor with user data

--- app/src/main/java/com/partum/tabsplit/utils/AuthSessionManager.kt
+++ app/src/main/java/com/partum/tabsplit/utils/AuthSessionManager.kt
@@ -5,16 +5,24 @@ import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import androidx.core.content.edit
+import com.google.gson.Gson
+import com.partum.tabsplit.data.model.User
 
-object TokenProvider {
+object AuthSessionManager {
     private const val PREFS_NAME = "auth_store"
     private const val JWT_KEY = "jwt_token"
+    private const val USER_KEY = "user_json"
 
     @Volatile
     var token: String? = null
         private set
 
+    @Volatile
+    var user: User? = null
+        private set
+
     private lateinit var prefs: SharedPreferences
+    private val gson = Gson()
 
     fun init(ctx: Context) {
         // Use EncryptedSharedPreferences for security
@@ -27,17 +35,32 @@ object TokenProvider {
         )
 
         token = prefs.getString(JWT_KEY, null)
+        prefs.getString(USER_KEY, null)?.let {
+            user = gson.fromJson<User>(it, User::class.java)
+        }
     }
 
-    fun saveToken(value: String?) {
-        token = value
+    fun save(token: String?, user: User?) {
+        this.token = token
+        this.user = user
 
         prefs.edit {
-            if (value == null) {
+            if (token == null) {
                 remove(JWT_KEY)
             } else {
-                putString(JWT_KEY, value)
+                putString(JWT_KEY, token)
             }
+
+            if (user == null) remove(USER_KEY) else putString(USER_KEY, gson.toJson(user))
         }
+
+    }
+
+    fun isValidSession(): Boolean {
+        return token != null && user != null
+    }
+
+    fun clear() {
+        save(null, null)
     }
 }
