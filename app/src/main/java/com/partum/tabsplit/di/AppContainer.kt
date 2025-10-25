5911c98afb545d2816ecbf9b6e31a36caeee923c
amochuko <7204868+amochuko@users.noreply.github.com>
2025-10-25 17:25:24 +0100

    update: add ExpenseRepo

--- app/src/main/java/com/partum/tabsplit/di/AppContainer.kt
+++ app/src/main/java/com/partum/tabsplit/di/AppContainer.kt
@@ -4,14 +4,17 @@ import android.app.Application
 import androidx.lifecycle.ViewModelProvider
 import com.partum.tabsplit.data.api.ApiClient
 import com.partum.tabsplit.data.api.SessionApi
+import com.partum.tabsplit.data.repository.ExpenseRepository
 import com.partum.tabsplit.data.repository.SessionRepository
 import com.partum.tabsplit.ui.auth.AuthViewModel
 import com.partum.tabsplit.ui.expense.ExpenseViewModel
 import com.partum.tabsplit.ui.participant.ParticipantViewModel
 import com.partum.tabsplit.utils.Config
+import com.partum.tabsplit.data.api.ExpenseApi
 
 interface IAppContainer {
     val sessionRepository: SessionRepository
+    val expenseRepository: ExpenseRepository
 }
 
 class AppContainer(app: Application) : IAppContainer {
@@ -20,15 +23,25 @@ class AppContainer(app: Application) : IAppContainer {
     private val sessionApi: SessionApi by lazy {
         ApiClient.create<SessionApi>(authViewModel.getToken(), Config.API_BASE_URL)
     }
+    private val expenseApi: ExpenseApi by lazy {
+        ApiClient.create<ExpenseApi>(null, Config.API_BASE_URL)
+    }
 
     // --- Repository
     override val sessionRepository: SessionRepository by lazy {
         SessionRepository(sessionApi)
     }
 
+    override val expenseRepository: ExpenseRepository by lazy {
+        ExpenseRepository(expenseApi)
+    }
+
     // --- ViewModels
     val authViewModel = AuthViewModel(app)
-//    val expenseViewModel = ExpenseViewModel(sessionRepository)
+//    val expenseViewModel = ExpenseViewModel(
+//        sessionRepository,
+//        expenseRepository
+//    )
 //    val participantViewModel = ParticipantViewModel(sessionRepository)
 
 
@@ -36,6 +49,7 @@ class AppContainer(app: Application) : IAppContainer {
     val viewModelFactory: ViewModelProvider.Factory by lazy {
         AppViewModelFactory(
             sessionRepository,
+            expenseRepository,
             authViewModel
         )
     }
